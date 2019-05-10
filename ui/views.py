4196c3e064fb4849cdebe7a716fe6d931b5539cb
Lucas Bode <lucas.bode@protolabs.com>
2019-05-10 08:43:55 -0500

    Moved order by logic to views.py

--- ui/views.py
+++ ui/views.py
@@ -12,7 +12,7 @@ def incident_doc(request: HttpRequest, incident_id: str):
     except Incident.DoesNotExist:
         raise Http404("Incident does not exist")
 
-    events = PinnedMessage.objects.filter(incident=incident)
+    events = PinnedMessage.objects.filter(incident=incident).order_by('timestamp')
     user_stats = UserStats.objects.filter(incident=incident).order_by('-message_count')[:5]
 
     return render(request, template_name='incident_doc.html', context={
