4d9b9acf7f3e85bfcca3b3f9e5c0b3c29502162f
Lucas Bode <lucas.bode@protolabs.com>
2019-05-21 15:44:39 -0500

    Updated status page mention to include a configured value.

--- slack/keyword_handlers.py
+++ slack/keyword_handlers.py
@@ -1,4 +1,5 @@
 from core.models.incident import Incident
+from django.conf import settings
 
 from slack.models import CommsChannel
 from slack.decorators import keyword_handler
@@ -9,4 +10,4 @@ logger = logging.getLogger(__name__)
 
 @keyword_handler(['status page', 'statuspage'])
 def status_page_notification(comms_channel: CommsChannel, user: str, text: str, ts: str):
-    comms_channel.post_in_channel("ℹ️ You mentiond the Status Page - <http://google.com|here's the runbook> on how to put it up.")
+    comms_channel.post_in_channel(f"ℹ️ You mentioned the Status Page - <{settings.STATUS_PAGE}|here's the runbook> on how to put it up.")
