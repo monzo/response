fb3347e2927a10d42753ad5f55bb6cab6932eb5c
adamlphillips <adamphillips@monzo.com>
2019-06-24 13:27:07 +0100

    You should be authenticated to see any data

--- response/settings/base.py
+++ response/settings/base.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ]
 }
 
