514c4f1693ae996102f9c111a9b125794d1f73da
Miles Bryant <milesbryant@monzo.com>
2019-07-12 17:58:16 +0100

    Add shim for running e2e tests

---
+++ e2e/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SKIP_CLEANUP=${SKIP_CLEANUP:-}
+
+echo "🌐 Starting demo app..."
+docker-compose -f "demo/docker-compose.yaml" up -d
+
+function finish {
+    echo ""
+    echo "🔽 Cleaning up demo app .."
+    docker-compose -f "demo/docker-compose.yaml" down 
+    docker-compose -f "demo/docker-compose.yaml" rm 
+}
+
+if [[ -z "$SKIP_CLEANUP" ]]; then
+    trap finish EXIT
+fi
+
+
+echo ""
+echo "⏲️  Running e2e test container..."
+docker run -it --rm \
+    --name response-e2e \
+    -v "$(pwd)":/usr/src/response \
+    -w /usr/src/response/e2e \
+    python:3.7 \
+    bash _run_in_docker.sh
+
