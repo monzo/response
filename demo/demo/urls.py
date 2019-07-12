7ac3f369a991f6b165ee803862fcbb6588520a47
Miles Bryant <milesbryant@monzo.com>
2019-07-12 11:31:40 +0100

    Migrate django installation to new "demo" app

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('slack/', include('response.slack.urls')),
+    path('core/', include('response.core.urls')),
+    path('', include('response.ui.urls')),
 ]
