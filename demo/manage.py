7eaccd8b61dfaca85dfb1df0aeb45e9d0ccf75e3
Miles Bryant <milesbryant@monzo.com>
2019-07-12 11:22:56 +0100

    Create new demo app

--- demo/manage.py
+++ demo/manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'response.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
