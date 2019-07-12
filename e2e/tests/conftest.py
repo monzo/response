5d9c4b8275fcfb5600f78ca8863a191d1de8200d
Miles Bryant <milesbryant@monzo.com>
2019-07-12 17:58:16 +0100

    Add shim for running e2e tests

---
+++ e2e/tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+import requests
+
+
+from requests import Session
+from urllib.parse import urljoin
+
+class ResponseSession(Session):
+    def __init__(self, prefix_url="http://localhost:8000", *args, **kwargs):
+        super(ResponseSession, self).__init__(*args, **kwargs)
+        self.prefix_url = prefix_url
+
+    def request(self, method, url, *args, **kwargs):
+        url = urljoin(self.prefix_url, url)
+        return super(ResponseSession, self).request(method, url, *args, **kwargs)
+
+
+@pytest.fixture(scope="session")
+def server_url():
+    return "http://localhost:8000"
+
+@pytest.fixture(scope="session")
+def client(server_url):
+    s = ResponseSession(prefix_url=server_url)
+    return s
+
+@pytest.fixture(autouse=True,scope="session")
+def check_health(client):
+    r = client.get("admin/")
+    r.raise_for_status()
+
