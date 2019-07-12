2b8686b95b71bb679456baa2cea9e5926d56d857
Miles Bryant <milesbryant@monzo.com>
2019-07-12 12:17:45 +0100

    Consolidate Slack app into response

--- response/apps.py
+++ response/apps.py
@@ -3,3 +3,16 @@ from django.apps import AppConfig
 
 class ResponseConfig(AppConfig):
     name = 'response'
+
+    def ready(self):
+        from .slack import (settings,
+                       signals,
+                       action_handlers,
+                       event_handlers,
+                       incident_commands,
+                       keyword_handlers,
+                       incident_notifications,
+                       dialog_handlers,
+                       workflows)
+        if settings.PAGERDUTY_ENABLED:
+            from .slack.workflows import pagerduty
