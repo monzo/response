2fe93288449d0a04aa50ca38d3694edadad0188d
Miles Bryant <milesbryant@monzo.com>
2019-07-15 14:11:00 +0100

    build & push response to PyPI using Travis CI
    Also adds the README.md to the package description and bumps the version

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 import os
 from setuptools import find_packages, setup
 
-
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# load README.md
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='django-incident-response',
-    version='0.0.1-prerelease',
+    version='0.0.1a3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude="demo"),
     package_dir={"response": "response"},
     include_package_data=True,
