918202f6f8b1861130f13751060bbfbb6cfe0d8a
Miles Bryant <milesbryant@monzo.com>
2019-08-19 16:07:23 +0100

    Lambda won't serialize in migrations

--- response/core/models/timeline.py
+++ response/core/models/timeline.py
@@ -11,9 +11,7 @@ class TimelineEvent(models.Model):
 
     incident = models.ForeignKey(Incident, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(
-        null=False,
-        default=lambda: datetime.now(),
-        help_text="Time of when this event occurred.",
+        null=False, default=datetime.now, help_text="Time of when this event occurred."
     )
     text = models.TextField(help_text="Freeform text describing this event")
     event_type = models.CharField(
