b6df8fe35f89b87016bf133f506f85432800cd34
Matt Cottingham <mattcottingham@monzo.com>
2019-08-20 12:47:37 +0100

    Fix string format - exceptions were not being logged

--- response/slack/models/comms_channel.py
+++ response/slack/models/comms_channel.py
@@ -13,12 +13,14 @@ logger = logging.getLogger(__name__)
 
 class CommsChannelManager(models.Manager):
     def create_comms_channel(self, incident):
-        "Creates a comms channel in slack, and saves a reference to it in the DB"
+        """
+        Creates a comms channel in slack, and saves a reference to it in the DB
+        """
         try:
             name = f"inc-{100+incident.pk}"
             channel_id = settings.SLACK_CLIENT.get_or_create_channel(name, auto_unarchive=True)
         except SlackError as e:
-            logger.error('Failed to create comms channel {e}')
+            logger.error(f"Failed to create comms channel {e}")
 
         try:
             doc_url = urljoin(
@@ -28,7 +30,7 @@ class CommsChannelManager(models.Manager):
 
             settings.SLACK_CLIENT.set_channel_topic(channel_id, f"{incident.report} - {doc_url}")
         except SlackError as e:
-            logger.error('Failed to set channel topic {e}')
+            logger.error(f"Failed to set channel topic {e}")
 
         comms_channel = self.create(
             incident=incident,
