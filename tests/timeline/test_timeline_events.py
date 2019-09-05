c5c0743e0da5dc459264edff9febb805d85158b8
Miles Bryant <milesbryant@monzo.com>
2019-09-05 17:01:36 +0100

    Add timeline events on incident updates
    This populates the timeline when key fields in an incident are updated,
    e.g. "Incident lead was changed from Barry to Mary".

---
+++ tests/timeline/test_timeline_events.py
@@ -0,0 +1,72 @@
+import random
+
+import pytest
+from faker import Factory
+from response.core.models import TimelineEvent
+
+from tests.factories import ExternalUserFactory, IncidentFactory
+
+faker = Factory.create()
+
+
+@pytest.mark.django_db
+def test_update_incident_lead():
+    incident = IncidentFactory.create()
+    new_lead = ExternalUserFactory.create()
+
+    incident.lead = new_lead
+    incident.save()
+
+    event = TimelineEvent.objects.get(incident=incident, event_type="incident_update")
+    assert event.metadata["new_value"]["display_name"] == new_lead.display_name
+
+
+@pytest.mark.django_db
+def test_update_incident_report():
+    incident = IncidentFactory.create()
+    new_report = faker.paragraph(nb_sentences=3, variable_nb_sentences=True)
+
+    incident.report = new_report
+    incident.save()
+
+    event = TimelineEvent.objects.get(incident=incident, event_type="incident_update")
+    assert event.metadata["new_value"] == new_report
+
+
+@pytest.mark.django_db
+def test_update_incident_summary():
+    incident = IncidentFactory.create()
+    new_summary = faker.paragraph(nb_sentences=3, variable_nb_sentences=True)
+
+    incident.summary = new_summary
+    incident.save()
+
+    event = TimelineEvent.objects.get(incident=incident, event_type="incident_update")
+    assert event.metadata["new_value"] == new_summary
+
+
+@pytest.mark.django_db
+def test_update_incident_impact():
+    incident = IncidentFactory.create()
+    new_impact = faker.paragraph(nb_sentences=1)
+
+    incident.impact = new_impact
+    incident.save()
+
+    event = TimelineEvent.objects.get(incident=incident, event_type="incident_update")
+    assert event.metadata["new_value"] == new_impact
+
+
+@pytest.mark.django_db
+def test_update_incident_severity():
+    incident = IncidentFactory.create()
+    new_severity = str(random.randint(1, 4))
+
+    incident.severity = new_severity
+    incident.save()
+
+    event = TimelineEvent.objects.get(incident=incident, event_type="incident_update")
+    assert event.metadata["new_value"] == {
+        "id": new_severity,
+        "text": incident.severity_text(),
+    }
