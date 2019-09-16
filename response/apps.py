3a39ba6f7ccf476a2d5c9960e6d3484a166aeb58
Dan Palmer <dan@thread.com>
2019-09-16 11:15:41 +0100

    Lazily evaluate so that we know the file exists if manifested

--- response/apps.py
+++ response/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.conf import settings as site_settings
 from django.templatetags.static import static
+from django.utils.functional import lazy
 
 
 class ResponseConfig(AppConfig):
@@ -21,8 +22,15 @@ class ResponseConfig(AppConfig):
 
         self.configure_setting("RESPONSE_LOGIN_REQUIRED", True)
         self.configure_setting("RESPONSE_APP_NAME", "Response")
-        self.configure_setting("RESPONSE_FAVICON", static("images/favicon.png"))
-        self.configure_setting("RESPONSE_LOGO", static("images/response.png"))
+        self.configure_setting(
+            "RESPONSE_FAVICON", self.lazy_static_url("images/favicon.png")
+        )
+        self.configure_setting(
+            "RESPONSE_LOGO", self.lazy_static_url("images/response.png")
+        )
 
     def configure_setting(self, name, default):
         setattr(site_settings, name, getattr(site_settings, name, default))
+
+    def lazy_static_url(self, name):
+        return lazy(lambda: static(name))()
