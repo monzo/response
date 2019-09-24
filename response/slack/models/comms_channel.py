fbdc64417806d8e0c30bfa72cb4f61e0b3eaf09a
Miles Bryant <milesbryant@monzo.com>
2019-09-24 13:24:14 +0100

    Add channel_name field

--- response/slack/models/comms_channel.py
+++ response/slack/models/comms_channel.py
@@ -49,15 +49,13 @@ class CommsChannel(models.Model):
     objects = CommsChannelManager()
     incident = models.ForeignKey(Incident, on_delete=models.CASCADE)
     channel_id = models.CharField(max_length=20, null=False)
+    channel_name = models.CharField(max_length=80, null=False)
 
     def post_in_channel(self, message: str):
         settings.SLACK_CLIENT.send_message(self.channel_id, message)
 
     def rename(self, new_name):
         settings.SLACK_CLIENT.rename_channel(self.channel_id, new_name)
-
-    def channel_name(self):
-        return settings.SLACK_CLIENT.get_channel_name(self.channel_id)
 
     def __str__(self):
         return self.incident.report
