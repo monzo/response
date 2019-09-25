45241f3d28c2baaa7a38f3291a01fa3e0d8156e4
Miles Bryant <milesbryant@monzo.com>
2019-09-25 10:28:31 +0100

    Test channel renaming

---
+++ tests/slack/test_channel_rename.py
@@ -0,0 +1,13 @@
+import pytest
+
+from response.slack.event_handlers import handle_channel_rename
+
+from tests.factories import IncidentFactory
+
+
+@pytest.mark.django_db
+def test_handle_channel_rename(mock_slack):
+    incident = IncidentFactory.create()
+
+    handle_channel_rename(incident, {"channel": {"name": "new-channel-name"}})
+    assert incident.comms_channel().channel_name == "new-channel-name"
