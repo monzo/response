3e1ff04fb72cc3926c8dd57be63bf2c5a84e8d13
Dan Palmer <dan@thread.com>
2019-09-27 16:18:11 +0100

    Remove response settings in favour of many more template customisation points

--- response/apps.py
+++ response/apps.py
@@ -1,7 +1,5 @@
 from django.apps import AppConfig
 from django.conf import settings as site_settings
-from django.templatetags.static import static
-from django.utils.functional import lazy
 
 
 class ResponseConfig(AppConfig):
@@ -20,17 +18,6 @@ class ResponseConfig(AppConfig):
 
         from .core import signals as core_signals  # noqa: F401
 
-        self.configure_setting("RESPONSE_LOGIN_REQUIRED", True)
-        self.configure_setting("RESPONSE_APP_NAME", "Response")
-        self.configure_setting(
-            "RESPONSE_FAVICON", self.lazy_static_url("images/favicon.png")
+        site_settings.RESPONSE_LOGIN_REQUIRED = getattr(
+            site_settings, "RESPONSE_LOGIN_REQUIRED", True
         )
-        self.configure_setting(
-            "RESPONSE_LOGO", self.lazy_static_url("images/response.png")
-        )
-
-    def configure_setting(self, name, default):
-        setattr(site_settings, name, getattr(site_settings, name, default))
-
-    def lazy_static_url(self, name):
-        return lazy(lambda: static(name))()
