8e2e40d07d6da7f34d6545961ec47d0fa620e3ec
Miles Bryant <milesbryant@monzo.com>
2019-10-22 11:07:38 +0100

    Allows disabling sanitization with RESPONSE_SANITIZE_USER_INPUT (#171)
    * Allows disabling sanitization with RESPONSE_SANITIZE_USER_INPUT

--- response/core/util.py
+++ response/core/util.py
@@ -1,10 +1,11 @@
 import bleach
 import bleach_whitelist
+from django.conf import settings
 
 
 def sanitize(string):
     # bleach doesn't handle None so let's not pass it
-    if string:
+    if string and getattr(settings, "RESPONSE_SANITIZE_USER_INPUT", True):
         return bleach.clean(
             string,
             tags=bleach_whitelist.markdown_tags,
