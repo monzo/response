5e6eca7e5effba7862c38a903f76e0aaa52d2f53
Matt Cottingham <mattcottingham@monzo.com>
2019-11-06 14:07:29 +0000

    Allow event signals to be configured

--- response/core/signals.py
+++ response/core/signals.py
@@ -1,42 +1,61 @@
 from datetime import datetime
 
-from response.core.models import Action, Event, Incident
-from response.core.serializers import ActionSerializer, IncidentSerializer
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 import logging
 logger = logging.getLogger(__name__)
 
+import os
 
-@receiver(post_save, sender=Action)
-def emit_action_event(sender, instance: Action, **kwargs):
-    logger.info(f"Handling post_save for action: {instance}")
+from response.core.models import Action, Event, Incident
+from response.core.serializers import ActionSerializer, IncidentSerializer
 
-    event = Event()
-    event.event_type = Event.ACTION_EVENT_TYPE
-
-    # Event payload should be a dict for serializing to JSON.
-    event.payload = ActionSerializer(instance).data
-    event.payload["incident_id"] = instance.incident.pk
-    # TODO: define a serializer that elides this field (otherwise
-    # it is returned in incident responses too)
-    if "details_ui" in event.payload: del event.payload["details_ui"]
-
-    event.timestamp = datetime.now(tz=None)
-    event.save()
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.module_loading import import_string
 
 
-@receiver(post_save, sender=Incident)
-def emit_incident_event(sender, instance: Incident, **kwargs):
-    logger.info(f"Handling post_save for incident: {instance}")
+class ActionEventHandler():
 
-    event = Event()
-    event.event_type = Event.INCIDENT_EVENT_TYPE
+    def handle(sender, instance: Action, **kwargs):
+        logger.info(f"Handling post_save for action: {instance}")
 
-    # Event payload should be a dict for serializing to JSON.
-    event.payload = IncidentSerializer(instance).data
+        event = Event()
+        event.event_type = Event.ACTION_EVENT_TYPE
 
-    event.timestamp = datetime.now(tz=None)
-    event.save()
+        # Event payload should be a dict for serializing to JSON.
+        event.payload = ActionSerializer(instance).data
+        event.payload["incident_id"] = instance.incident.pk
+        if "details_ui" in event.payload: del event.payload["details_ui"]
+
+        event.timestamp = datetime.now(tz=None)
+        event.save()
+
+
+class IncidentEventHandler():
+
+    def handle(sender, instance: Incident, **kwargs):
+        logger.info(f"Handling post_save for incident: {instance}")
+
+        event = Event()
+        event.event_type = Event.INCIDENT_EVENT_TYPE
+
+        # Event payload should be a dict for serializing to JSON.
+        event.payload = IncidentSerializer(instance).data
+        # Actions generate their own events, no need to duplicate them here.
+        if "action_items" in event.payload: del event.payload["action_items"]
+
+        event.timestamp = datetime.now(tz=None)
+        event.save()
+
+
+if hasattr(settings, "ACTION_EVENT_HANDLER_CLASS"):
+    cls = import_string(settings.ACTION_EVENT_HANDLER_CLASS)
+    post_save.connect(cls.handle, sender=Action)
+else:
+    post_save.connect(ActionEventHandler.handle, sender=Action)
+
+if hasattr(settings, "INCIDENT_EVENT_HANDLER_CLASS"):
+    cls = import_string(settings.INCIDENT_EVENT_HANDLER_CLASS)
+    post_save.connect(cls.handle, sender=Incident)
+else:
+    post_save.connect(IncidentEventHandler.handle, sender=Incident)
