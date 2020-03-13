ca0bc4691143b340a5f53e7dfcc36d540c69cb46
Stephan Diederich <stephan.diederich@gmail.com>
2020-03-13 12:06:20 +0100

    fix comparison
    * fix bracket placement -> We wanna make sure to check if
    severity is not None, and then compare to the level
    * severity is a string, convert to int before comparing
    
    ### Testplan
    
    * open home
    * make sure it doesn't error out

--- response/core/models/incident.py
+++ response/core/models/incident.py
@@ -115,7 +115,7 @@ class Incident(models.Model):
             if sev_id == self.severity:
                 return text
         return None
-
+    
     def severity_emoji(self):
         if not self.severity:
             return "☁️"
@@ -141,7 +141,7 @@ class Incident(models.Model):
     def badge_type(self):
         if self.is_closed():
             return "badge-success"
-        elif (self.severity and self.severity) < 3:
+        elif self.severity and int(self.severity) < 3:
             return "badge-danger"
         return "badge-warning"
 
