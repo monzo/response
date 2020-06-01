0f9931b58232391f31d70f65b937e7d894a61d82
Stephan Diederich <stephan.diederich@gmail.com>
2020-06-01 10:19:08 +0200

    Add a "home", show list of incidents & show actions in incidents (#204)
    Co-authored-by: George MacRorie <me@georgemac.com>

--- response/ui/urls.py
+++ response/ui/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("incident/<int:incident_id>/", views.incident_doc, name="incident_doc")
+    path("", views.home, name="home"),
+    path("incident/<int:incident_id>/", views.incident_doc, name="incident_doc"),
 ]
--- response/ui/views.py
+++ response/ui/views.py
@@ -1,9 +1,15 @@
 from django.http import Http404, HttpRequest
 from django.shortcuts import render
 
-from response.core.models import Incident
+from response.core.models import Action, Incident
 from response.decorators import response_login_required
 from response.slack.models import PinnedMessage, UserStats
+
+
+@response_login_required
+def home(request: HttpRequest):
+    incidents = Incident.objects.all
+    return render(request, template_name="home.html", context={"incidents": incidents})
 
 
 @response_login_required
@@ -14,12 +20,17 @@ def incident_doc(request: HttpRequest, incident_id: str):
         raise Http404("Incident does not exist")
 
     events = PinnedMessage.objects.filter(incident=incident).order_by("timestamp")
+    actions = Action.objects.filter(incident=incident).order_by("created_date")
     user_stats = UserStats.objects.filter(incident=incident).order_by("-message_count")[
         :5
     ]
-
     return render(
         request,
         template_name="incident_doc.html",
-        context={"incident": incident, "events": events, "user_stats": user_stats},
+        context={
+            "incident": incident,
+            "events": events,
+            "actions": actions,
+            "user_stats": user_stats,
+        },
     )
